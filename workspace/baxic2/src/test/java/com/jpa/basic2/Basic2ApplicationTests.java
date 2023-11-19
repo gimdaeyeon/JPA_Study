382df632a5ea91d5d447f79d2e625b53d44fb1ad
gimdaeyeon <rlaeodus1306@naver.com>
2023-11-19 21:54:14 +0900

    Rename: 패키지 이름 변경

--- workspace/baxic2/src/test/java/com/jpa/basic2/Basic2ApplicationTests.java
+++ workspace/baxic2/src/test/java/com/jpa/basic2/Basic2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jpa.baxic2;
+package com.jpa.basic2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Baxic2ApplicationTests {
+class Basic2ApplicationTests {
 
     @Test
     void contextLoads() {
