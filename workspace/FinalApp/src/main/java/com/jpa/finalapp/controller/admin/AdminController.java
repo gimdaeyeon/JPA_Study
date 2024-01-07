d33077378b6c9ea9bebec7125f78262e8bf23a01
gimdaeyeon <rlaeodus1306@naver.com>
2024-01-07 23:53:43 +0900

    Study: JPA finalAPP controller경로 설정 중

---
+++ workspace/FinalApp/src/main/java/com/jpa/finalapp/controller/admin/AdminController.java
@@ -0,0 +1,14 @@
+package com.jpa.finalapp.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/*")
+public class AdminController {
+    @GetMapping("/product/register")
+    public String produceRegister(){
+        return "admin/product_register";
+    }
+}
