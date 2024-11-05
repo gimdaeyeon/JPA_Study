a2306c86cb106c14e1814b4826c01a5f37044558
gimdaeyeon <rlaeodus1306@naver.com>
2024-11-05 22:45:02 +0900

    Study: 프록시 팩토리 학습, 예제 적용

---
+++ workspace/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,24 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//        invocation.proceed() -> target 클래스를 호출하고 그 결과를 받는다.
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("TimeProxy 종료 resultTime = {}", resultTime);
+
+        return result;
+    }
+}
